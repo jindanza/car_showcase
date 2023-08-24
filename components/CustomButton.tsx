25dd2dbd73cbd1583b65b9732f697e75ad8bbe67
jindanza <jindanza7@gmail.com>
2023-08-25 00:04:13 +0800

    feat: rapid api, carcard, footer, searchbar

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,11 +2,17 @@
 
 import { CustomButtonProps } from '@/types'
 import React from 'react'
+import Image from 'next/image'
 
-const CustomButton = ({title, containerStyle, handleClick, btnType}: CustomButtonProps) => {
+const CustomButton = ({title, containerStyle, handleClick, btnType, textStyles, rightIcon}: CustomButtonProps) => {
   return (
     <button disabled={false} type={btnType || 'button'} className={`custom-btn ${containerStyle}`} onClick={handleClick}>
-        <span className='flex-1'>{title}</span>
+        <span className={`flex-1 ${textStyles}`}>{title}</span>
+        {rightIcon && (
+          <div className='relative w-6 h-6'>
+            <Image src={rightIcon} alt='right icon' fill className='object-contain'/>
+          </div>
+        )}
     </button>
   )
 }
