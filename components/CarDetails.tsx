76d69ff4b7a276f43c9ef792a12da3e3e9da4fda
jindanza <jindanza7@gmail.com>
2023-08-27 23:42:56 +0800

    feat: custom image of car

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import Image from 'next/image'
 import { Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
+import { generateCarImageUrl } from '@/utils'
 
 interface CarDetailsProps {
   isOpen: boolean
@@ -29,17 +30,17 @@ const CarDetails = ({isOpen, closeModal, car} : CarDetailsProps) => {
                   </button>
                   <div className='flex-1 flex flex-col gap-3'>
                     <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
-                      <Image src="/hero.png" alt='Car Model' fill priority className='object-contain'/>
+                      <Image src={generateCarImageUrl(car)} alt='Car Model' fill priority className='object-contain'/>
                     </div>
                     <div className='flex gap-3'>
                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                        <Image src="/hero.png" alt='Car Model' fill priority className='object-contain'/>
+                        <Image src={generateCarImageUrl(car, '29')} alt='Car Model' fill priority className='object-contain'/>
                       </div>
                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                        <Image src="/hero.png" alt='Car Model' fill priority className='object-contain'/>
+                        <Image src={generateCarImageUrl(car, '33')} alt='Car Model' fill priority className='object-contain'/>
                       </div>
                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                        <Image src="/hero.png" alt='Car Model' fill priority className='object-contain'/>
+                        <Image src={generateCarImageUrl(car, '13')} alt='Car Model' fill priority className='object-contain'/>
                       </div>
                     </div>
                   </div>
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
     const headers = {
         'X-RapidAPI-Key': 'bb5f512046msh8b1f925ebe3cc27p168035jsn8e9819841048',
@@ -25,3 +27,18 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   
     return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+  
+    url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append('angle', `${angle}`);
+  
+    return `${url}`;
+} 
