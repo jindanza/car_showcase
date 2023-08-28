deeb8ddac02ae6761be7eb1a46975b7e3a46f473
jindanza <jindanza7@gmail.com>
2023-08-28 23:13:20 +0800

    feat: can search car

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+    const {manufacturer, year, model, limit, fuel} = filters
+
     const headers = {
         'X-RapidAPI-Key': 'bb5f512046msh8b1f925ebe3cc27p168035jsn8e9819841048',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {headers: headers,})
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {headers: headers,})
 
     const result = await response.json()
 
